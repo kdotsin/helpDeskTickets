ee4a1220efdaff8131d20028952c4aa392d71e06
Richard Sin <sin.richard6@gmail.com>
2024-04-29 19:35:31 -0400

    please

--- helpDeskTickets/urls.py
+++ helpDeskTickets/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.urls import path, include
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('ticket.urls')),
+    path('', RedirectView.as_view(url='/api/', permanent=False)),
 ]
