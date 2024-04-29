40ada1d7683cc8b61074d853241f129dd0603a2b
Richard Sin <sin.richard6@gmail.com>
2024-04-29 19:08:04 -0400

    minor fix

--- helpDeskTickets/urls.py
+++ helpDeskTickets/urls.py
@@ -16,13 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
-
-def home_view(request):
-    return HttpResponse("Welcome to the Zealthy's Help Desk Ticket System!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('ticket.urls')),
-    path('', home_view, name='home'),
 ]
