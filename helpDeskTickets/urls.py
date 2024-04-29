f809ae4c8c8c012ce559f2e485b06d606c9419d7
Richard Sin <sin.richard6@gmail.com>
2024-04-29 19:09:54 -0400

    trying this out

--- helpDeskTickets/urls.py
+++ helpDeskTickets/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path, include, re_path
 from django.contrib import admin
-from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('ticket.urls')),
+    re_path(r'^$', lambda request: redirect('admin/', permanent=False)),
 ]
